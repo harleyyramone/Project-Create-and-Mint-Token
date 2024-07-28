f89016a04c0fc09ead55adb313b035cb83cfa4cb
ShankS <93700303+harleyyramone@users.noreply.github.com>
2024-07-28 22:51:35 +0800

    Update Code.sol

--- Code.sol
+++ Code.sol
@@ -1,1 +1,20 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("MyToken", "MTK") Ownable(msg.sender) {}
+
+    // Mint function that can only be called by the owner
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Burn function that can be called by any user
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
